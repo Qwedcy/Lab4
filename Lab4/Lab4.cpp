d32f9b672875404c22505ad6969a9b22238adf5c
Oleksandr <114135649+Qwedcy@users.noreply.github.com>
2022-11-17 14:30:05 +0200

    Initial commit

---
+++ Lab4/Lab4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Ukrainian");
+    double b[8];
+	int i;
+    cout << "Вкажiть 8 елементiв\n";
+	   for (i = 0; i < 8; i++)
+		cin >> b[i];
+    cout << "Елементи масиву A:\n";
+	for (i = 0; i < 8; i++)
+		cout << "b[" << i << "]=" << b[i] << endl;
+    double a[9];
+	   for (i = 0; i < 9; i++)
+    a[i] = ((b[i] + b[i + 1]) / 2);
+    cout << "Елементи масиву B:\n";
+	   for (i = 0; i < 9; i++)
+	cout << "a[" << i << "]=" << a[i] << endl;
+
+	return 0;
+
+}
